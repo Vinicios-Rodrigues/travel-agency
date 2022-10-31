09bb4088eaa999862f85a58947278d0b62294329
viniciospereirarodrigues@gmail.com <viniciospereirarodrigues@gmail.com>
2022-10-30 22:05:11 -0300

    estilo global e ajustes no app/index

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Header } from "./components/Header/Header";
+import { Main } from "./components/Main/Main";
+import "./styles/globalStyle.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
